ff649a9589afb241707561d3eee5439e840358eb
Austin Seipp <mad.one@gmail.com>
2012-09-02 18:38:28 -0500

    Use idiomatic type
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- bspatch.h
+++ bspatch.h
@@ -3,8 +3,8 @@
 
 typedef uint8_t u_char;
 
-bool valid_header(unsigned char* patchf);
-ssize_t bspatch_newsize(unsigned char* patch);
+bool valid_header(u_char* patchf);
+ssize_t bspatch_newsize(u_char* patch);
 int bspatch(u_char* oldf,   ssize_t   oldf_sz,
             u_char* patchf, ssize_t patchf_sz,
             u_char* newf);
