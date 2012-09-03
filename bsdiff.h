676aa70dc18e7d438dd99b35f2f1e8f7525faccd
Austin Seipp <mad.one@gmail.com>
2012-09-03 14:42:21 -0500

    Fix clang typedef-redeclaration warnings
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- bsdiff.h
+++ bsdiff.h
@@ -1,11 +1,14 @@
-#ifndef _BSDIFF_H_
-#define _BSDIFF_H_
+#ifndef _MINIBSDIFF_H_
+#define _MINIBSDIFF_H_
 
+#ifndef _MINIBSDIFF_U_CHAR_T_
+#define _MINIBSDIFF_U_CHAR_T_
 typedef uint8_t u_char;
+#endif
 
 /* NOTA BENE: patch must be at least be 'newsize' in length! */
 int bsdiff(u_char* old, off_t oldsize,
            u_char* new, off_t newsize,
            u_char* patch);
 
-#endif /* _BSDIFF_H_ */
+#endif /* _MINIBSDIFF_H_ */
--- bspatch.h
+++ bspatch.h
@@ -1,7 +1,10 @@
-#ifndef _BSPATCH_H_
-#define _BSPATCH_H_
+#ifndef _MINIBSPATCH_H_
+#define _MINIBSPATCH_H_
 
+#ifndef _MINIBSDIFF_U_CHAR_T_
+#define _MINIBSDIFF_U_CHAR_T_
 typedef uint8_t u_char;
+#endif
 
 bool valid_header(u_char* patchf);
 ssize_t bspatch_newsize(u_char* patch);
@@ -9,4 +12,4 @@ int bspatch(u_char* oldf,   ssize_t   oldf_sz,
             u_char* patchf, ssize_t patchf_sz,
             u_char* newf);
 
-#endif /* _BSPATCH_H_ */
+#endif /* _MINIBSPATCH_H_ */
