e7a2e1bb6c9d860bdfb6c902b53fe091ab0cac2b
Austin Seipp <aseipp@pobox.com>
2014-04-14 20:12:58 -0500

    Add release.nix for Hydra
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

---
+++ release.nix
@@ -0,0 +1,47 @@
+{ minibsdiff ? { outPath = ./.; revCount = 0; shortRev = "abcdef"; rev = "HEAD"; }
+, officialRelease ? false
+}:
+
+let
+  pkgs = import <nixpkgs> { };
+
+  systems = [ "x86_64-linux" ];
+
+  version = "0.0.1" + (pkgs.lib.optionalString (!officialRelease)
+    "-pre${toString minibsdiff.revCount}_${minibsdiff.shortRev}");
+in
+rec {
+
+  ## -- Tarballs ---------------------------------------------------------------
+  tarball = pkgs.releaseTools.sourceTarball {
+    name = "minibsdiff-tarball";
+    src  = minibsdiff;
+    inherit version;
+    officialRelease = true; # hack
+    buildInputs = [ pkgs.git pkgs.xz ];
+
+    distPhase = ''
+      relname=minibsdiff-${version}
+      mkdir ../$relname
+      cp -prd . ../$relname
+      rm -rf ../$relname/.git ../$relname/svn-revision
+
+      mkdir $out/tarballs
+      tar cvfJ $out/tarballs/$relname.tar.xz -C .. $relname
+    '';
+  };
+
+  ## -- Build ------------------------------------------------------------------
+  build = pkgs.lib.genAttrs systems (system:
+    with import <nixpkgs> { inherit system; };
+
+    releaseTools.nixBuild {
+      name = "minibsdiff";
+      src  = tarball;
+      enableParallelBuilding = true;
+      doCheck = false;
+
+      installPhase = "make install PREFIX=$out";
+    }
+  );
+}
